e71905d2fdf8aa3886684f4d7d8371e9eccfc3d2
Raffael <raffael.imboden@students.hevs.ch>
2021-05-27 23:10:22 +0200

    UI EntryDetails

--- app/src/main/java/com/example/agriculturetimemanagement/ui/EntryDetails.java
+++ app/src/main/java/com/example/agriculturetimemanagement/ui/EntryDetails.java
@@ -1,4 +1,15 @@
 package com.example.agriculturetimemanagement.ui;
 
-public class EntryDetails {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.agriculturetimemanagement.R;
+
+public class EntryDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_details);
+    }
 }
