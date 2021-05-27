c54b0c0792e1ae035cfaf258c5485c59caf0d3f4
danielphpmaster <31928601+danielphpmaster@users.noreply.github.com>
2021-05-27 23:49:41 +0200

    Add MainActivity.java and activity_main.xml

--- app/src/main/java/com/example/agriculturetimemanagement/ui/MainActivity.java
+++ app/src/main/java/com/example/agriculturetimemanagement/ui/MainActivity.java
@@ -1,16 +1,84 @@
 package com.example.agriculturetimemanagement.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 
 import com.example.agriculturetimemanagement.R;
+import com.example.agriculturetimemanagement.adapter.RecyclerAdapter;
+import com.example.agriculturetimemanagement.database.entity.EntryEntity;
+import com.example.agriculturetimemanagement.util.RecyclerViewItemClickListener;
+import com.example.agriculturetimemanagement.viewmodel.EntryListViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private List<EntryEntity> entries;
+    private RecyclerAdapter recyclerAdapter;
+    private EntryListViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle("Entries");
+
+        RecyclerView recyclerView = findViewById(R.id.entriesRecyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        entries = new ArrayList<>();
+        recyclerAdapter = new RecyclerAdapter(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Log.d(TAG, "clicked position: " + position);
+                Log.d(TAG, "clicked on: " + entries.get(position).toString());
+
+                Intent intent = new Intent(MainActivity.this, EntryDetails.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.putExtra("entryId", entries.get(position).getId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+                Log.d(TAG, "longClicked position: " + position);
+                Log.d(TAG, "longClicked on: " + entries.get(position).toString());
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, EntryDetails.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NO_HISTORY);
+            startActivity(intent);
+        });
+
+        recyclerView.setAdapter(recyclerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
